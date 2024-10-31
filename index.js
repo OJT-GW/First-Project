e879bd69568149b985179e369eac147d381be034
vanhman03 <82196335+vanhman03@users.noreply.github.com>
2024-10-31 15:20:02 +0700

    [Front-endFuctionality3]: Update UI for Login Page (#2)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 (function () {
-  console.log("Start file login with firebase");
   // Initialize Firebase
   var config = {
     apiKey: "xxxxx",
@@ -15,10 +14,8 @@
   //Google singin provider
   var ggProvider = new firebase.auth.GoogleAuthProvider();
   //Facebook singin provider
-  var fbProvider = new firebase.auth.FacebookAuthProvider();
   //Login in variables
   const btnGoogle = document.getElementById("btnGoogle");
-  const btnFaceBook = document.getElementById("btnFacebook");
 
   //Sing in with Google
   btnGoogle.addEventListener(
@@ -39,36 +36,4 @@
     },
     false
   );
-
-  //Sing in with Facebook
-  btnFaceBook.addEventListener(
-    "click",
-    (e) => {
-      firebase
-        .auth()
-        .signInWithPopup(fbProvider)
-        .then(function (result) {
-          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          console.log("User>>Facebook>", user);
-          // ...
-          userId = user.uid;
-        })
-        .catch(function (error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-          console.error("Error: hande error here>Facebook>>", error.code);
-        });
-    },
-    false
-  );
-  //jquery
 })();
